5e8e237663882d5cc3112174eb24f3188ee07cb4
Fernando Guerreo Campo <fernandoguerreroc65@gmail.com>
2022-11-30 22:03:57 -0500

    change setMap fucntion

--- src/utilities/todos.utilities.ts
+++ src/utilities/todos.utilities.ts
@@ -5,9 +5,9 @@ const todosMap: Todo = new Map()
 
 export const setTodosMapOnLocalStorage = async (): Promise<void> => {
   const { data } = await fetchTodos()
-  data.map((item) => {
-    return todosMap.set(item.id, item)
-  })
+  for (const item of data) {
+    todosMap.set(item.id, item)
+  }
 
   localStorage.testMap = JSON.stringify(Array.from(todosMap.entries()))
   // const hola = new Map(JSON.parse(localStorage.testMap))
